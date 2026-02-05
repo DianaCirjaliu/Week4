6f0a209c617cd8290e3456ebae8bdd61f145320d
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-02-05 18:03:32 +0200

    feat(move objects):implemented some of the move objects logic-still not working

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 var sideBox = document.getElementById("sideBox");
 var popBtn = document.getElementById("popBtn");
 var closeBtn = document.getElementById("closeBtn");
+var default_size = 100;
 popBtn.onclick = function () { return sideBox.classList.add("active"); };
 closeBtn.onclick = function () { return sideBox.classList.remove("active"); };
 //main image logic
@@ -10,6 +11,7 @@ var clearImg = document.getElementById("clearBtn");
 var main = document.querySelector("main");
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var contor = 0;
 var images = [
     "./assets/1.png",
     "./assets/2.png",
@@ -24,18 +26,33 @@ function initCanvas() {
     }
 }
 function drawImg(src, x, y) {
+    if (!ctx)
+        return;
     var img = new Image();
-    img.src = images[Math.floor(Math.random() * images.length)];
+    img.src = src;
     img.onload = function () {
-        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, x, y, 100, 100);
+        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, x, y, default_size, default_size);
     };
+}
+function render() {
+    if (!ctx)
+        return;
+    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+    savedImg.forEach(function (img) {
+        drawImg(img.src, img.x, img.y);
+        if (img.selected) {
+            ctx.strokeStyle = "pink";
+            ctx === null || ctx === void 0 ? void 0 : ctx.strokeRect(img.x, img.y, default_size, default_size);
+        }
+    });
 }
 function addRandomImg() {
     var random = images[Math.floor(Math.random() * images.length)];
-    var x = Math.random() * (canvas.width - 100);
-    var y = Math.random() * (canvas.height - 100);
-    var newImg = { src: random, x: x, y: y };
+    var x = Math.random() * (canvas.width - default_size);
+    var y = Math.random() * (canvas.height - default_size);
+    var newImg = { src: random, x: x, y: y, selected: false, poz: contor };
     savedImg.push(newImg);
+    contor++;
     localStorage.setItem("savedImg", JSON.stringify(savedImg));
     drawImg(newImg.src, newImg.x, newImg.y);
 }
@@ -52,6 +69,7 @@ function clearCanvas() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     savedImg = [];
     localStorage.removeItem("savedImg");
+    contor = 0;
 }
 addImg.onclick = addRandomImg;
 clearImg.onclick = clearCanvas;
@@ -73,3 +91,49 @@ function updateState() {
 window.addEventListener("scroll", updateState);
 window.addEventListener("resize", updateState);
 updateState();
+//move objects logic
+var leftBtn = document.getElementById("leftBtn");
+var rightBtn = document.getElementById("rightBtn");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+function selectImg(mouseX, mouseY) {
+    //reset all the selection before
+    savedImg.forEach(function (img) { return (img.selected = false); });
+    for (var i = savedImg.length - 1; i >= 0; i--) {
+        var img = savedImg[i];
+        if (img) {
+            if (mouseX >= (img === null || img === void 0 ? void 0 : img.x) &&
+                mouseX <= (img === null || img === void 0 ? void 0 : img.x) + default_size &&
+                mouseY >= img.y &&
+                mouseY <= img.y + default_size) {
+                img.selected = true;
+                render();
+                console.log("".concat(i));
+                break;
+            }
+        }
+    }
+}
+canvas.addEventListener("click", function (e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    selectImg(x, y);
+});
+function moveImg(dx, dy) {
+    var _a;
+    var img;
+    for (var i = 0; i < savedImg.length; i++) {
+        if (((_a = savedImg[i]) === null || _a === void 0 ? void 0 : _a.selected) === true) {
+            img = savedImg[i];
+        }
+    }
+    if (img) {
+        img.x += dx;
+        img.y += dy;
+        render();
+    }
+}
+leftBtn === null || leftBtn === void 0 ? void 0 : leftBtn.addEventListener('click', function () { return moveImg(-10, 0); });
+rightBtn === null || rightBtn === void 0 ? void 0 : rightBtn.addEventListener('click', function () { return moveImg(10, 0); });
+upBtn === null || upBtn === void 0 ? void 0 : upBtn.addEventListener('click', function () { return moveImg(0, -10); });
+downBtn === null || downBtn === void 0 ? void 0 : downBtn.addEventListener('click', function () { return moveImg(0, 10); });
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 const sideBox = document.getElementById("sideBox") as HTMLElement;
 const popBtn = document.getElementById("popBtn") as HTMLButtonElement;
 const closeBtn = document.getElementById("closeBtn") as HTMLButtonElement;
+const default_size = 100;
 
 popBtn.onclick = () => sideBox.classList.add("active");
 closeBtn.onclick = () => sideBox.classList.remove("active");
@@ -12,6 +13,7 @@ const clearImg = document.getElementById("clearBtn") as HTMLButtonElement;
 const main = document.querySelector("main") as HTMLElement;
 const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
+let contor = 0;
 
 const images: string[] = [
   "./assets/1.png",
@@ -24,6 +26,8 @@ interface Img {
   src: string;
   x: number;
   y: number;
+  selected: boolean;
+  poz: number;
 }
 
 let savedImg: Img[] = [];
@@ -36,21 +40,36 @@ function initCanvas() {
 }
 
 function drawImg(src: string, x: number, y: number) {
+  if(!ctx) return;
   const img = new Image();
-  img.src = images[Math.floor(Math.random() * images.length)]!;
+  img.src = src;
   img.onload = () => {
-    ctx?.drawImage(img, x, y, 100, 100);
+    ctx?.drawImage(img, x, y, default_size, default_size);
   };
+}
+
+function render(){
+  if(!ctx) return;
+  ctx?.clearRect(0,0, canvas.width, canvas.height);
+  savedImg.forEach(img => {
+    drawImg(img.src, img.x, img.y);
+    if(img.selected){
+      ctx.strokeStyle = "pink";
+      ctx?.strokeRect(img.x, img.y, default_size, default_size);
+    }});
+
+  
 }
 
 function addRandomImg() {
   const random = images[Math.floor(Math.random() * images.length)]!;
 
-  const x = Math.random() * (canvas.width - 100);
-  const y = Math.random() * (canvas.height - 100);
+  const x = Math.random() * (canvas.width - default_size);
+  const y = Math.random() * (canvas.height - default_size);
 
-  const newImg = { src: random, x: x, y: y };
+  const newImg = { src: random, x: x, y: y, selected: false, poz: contor };
   savedImg.push(newImg);
+  contor++;
 
   localStorage.setItem("savedImg", JSON.stringify(savedImg));
   drawImg(newImg.src!, newImg.x, newImg.y);
@@ -71,6 +90,7 @@ function clearCanvas() {
   ctx?.clearRect(0, 0, canvas.width, canvas.height);
   savedImg = [];
   localStorage.removeItem("savedImg");
+  contor = 0;
 }
 
 addImg.onclick = addRandomImg;
@@ -100,3 +120,62 @@ window.addEventListener("scroll", updateState);
 window.addEventListener("resize", updateState);
 
 updateState();
+
+//move objects logic
+
+const leftBtn = document.getElementById("leftBtn");
+const rightBtn = document.getElementById("rightBtn");
+const upBtn = document.getElementById("upBtn");
+const downBtn = document.getElementById("downBtn");
+
+function selectImg(mouseX: number, mouseY: number) {
+  //reset all the selection before
+  savedImg.forEach((img) => (img.selected = false));
+
+  for (let i = savedImg.length - 1; i >= 0; i--) {
+    const img = savedImg[i];
+    if (img) {
+      if (
+        mouseX >= img?.x &&
+        mouseX <= img?.x + default_size &&
+        mouseY >= img.y &&
+        mouseY <= img.y + default_size
+      ) {
+        img.selected = true;
+        render();
+        console.log(`${i}`);
+        break;
+      }
+    }
+  }
+}
+
+canvas.addEventListener("click", (e: MouseEvent) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  selectImg(x, y);
+});
+
+function moveImg(dx: number, dy: number) {
+
+  let img: Img | undefined;
+
+  for (let i = 0; i < savedImg.length; i++){
+    if(savedImg[i]?.selected === true){
+      img = savedImg[i];
+
+    }
+  }
+  if (img) {
+    img.x += dx;
+    img.y += dy;
+    render();
+  }
+
+}
+
+
+leftBtn?.addEventListener('click',() => moveImg(-10,0));
+rightBtn?.addEventListener('click', () => moveImg(10,0));
+upBtn?.addEventListener('click', () => moveImg(0,-10));
+downBtn?.addEventListener('click', () => moveImg(0,10));
