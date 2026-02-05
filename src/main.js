0c30fa64c52e7e85a39c05df6f5acd5b16bc72b1
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-02-05 14:07:41 +0200

    reformat the code

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 //select components
-var sideBox = document.getElementById('sideBox');
-var popBtn = document.getElementById('popBtn');
-var closeBtn = document.getElementById('closeBtn');
-popBtn.onclick = function () { return sideBox.classList.add('active'); };
-closeBtn.onclick = function () { return sideBox.classList.remove('active'); };
+var sideBox = document.getElementById("sideBox");
+var popBtn = document.getElementById("popBtn");
+var closeBtn = document.getElementById("closeBtn");
+popBtn.onclick = function () { return sideBox.classList.add("active"); };
+closeBtn.onclick = function () { return sideBox.classList.remove("active"); };
 //main image logic
-var addImg = document.getElementById('addBtn');
-var clearImg = document.getElementById('clearBtn');
-var main = document.querySelector('main');
-var canvas = document.getElementById('myCanvas');
+var addImg = document.getElementById("addBtn");
+var clearImg = document.getElementById("clearBtn");
+var main = document.querySelector("main");
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var images = ['./assets/1.png', './assets/2.png', './assets/3.png', './assets/4.png'];
-;
+var images = [
+    "./assets/1.png",
+    "./assets/2.png",
+    "./assets/3.png",
+    "./assets/4.png",
+];
 var savedImg = [];
 function initCanvas() {
     if (main && canvas) {
@@ -32,12 +36,12 @@ function addRandomImg() {
     var y = Math.random() * (canvas.height - 100);
     var newImg = { src: random, x: x, y: y };
     savedImg.push(newImg);
-    localStorage.setItem('savedImg', JSON.stringify(savedImg));
+    localStorage.setItem("savedImg", JSON.stringify(savedImg));
     drawImg(newImg.src, newImg.x, newImg.y);
 }
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
     initCanvas();
-    var data = localStorage.getItem('savedImg');
+    var data = localStorage.getItem("savedImg");
     if (data) {
         savedImg = JSON.parse(data);
         savedImg.forEach(function (img) { return drawImg(img.src, img.x, img.y); });
@@ -47,13 +51,13 @@ addImg.onclick = addRandomImg;
 function clearCanvas() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
     savedImg = [];
-    localStorage.removeItem('savedImg');
+    localStorage.removeItem("savedImg");
 }
 addImg.onclick = addRandomImg;
 clearImg.onclick = clearCanvas;
-//info window logic 
-var scrollEl = document.getElementById('scrollPos');
-var dimensionEl = document.getElementById('innerDim');
+//info window logic
+var scrollEl = document.getElementById("scrollPos");
+var dimensionEl = document.getElementById("innerDim");
 function updateState() {
     var scrollX = Math.round(window.scrollX);
     var scrollY = Math.round(window.scrollY);
@@ -66,6 +70,6 @@ function updateState() {
         dimensionEl.innerText = "".concat(width, "px x ").concat(height, "px");
     }
 }
-window.addEventListener('scroll', updateState);
-window.addEventListener('resize', updateState);
+window.addEventListener("scroll", updateState);
+window.addEventListener("resize", updateState);
 updateState();
--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,102 @@
 //select components
-const sideBox = document.getElementById('sideBox') as HTMLElement;
-const popBtn = document.getElementById('popBtn') as HTMLButtonElement;
-const closeBtn = document.getElementById('closeBtn') as HTMLButtonElement;
+const sideBox = document.getElementById("sideBox") as HTMLElement;
+const popBtn = document.getElementById("popBtn") as HTMLButtonElement;
+const closeBtn = document.getElementById("closeBtn") as HTMLButtonElement;
 
-popBtn.onclick = () => sideBox.classList.add('active');
-closeBtn.onclick = () => sideBox.classList.remove('active');
-
+popBtn.onclick = () => sideBox.classList.add("active");
+closeBtn.onclick = () => sideBox.classList.remove("active");
 
 //main image logic
-const addImg = document.getElementById('addBtn') as HTMLButtonElement;
-const clearImg = document.getElementById('clearBtn') as HTMLButtonElement;
-const main = document.querySelector('main') as HTMLElement;
-const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+const addImg = document.getElementById("addBtn") as HTMLButtonElement;
+const clearImg = document.getElementById("clearBtn") as HTMLButtonElement;
+const main = document.querySelector("main") as HTMLElement;
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
-const images: string[] = ['./assets/1.png', './assets/2.png', './assets/3.png', './assets/4.png'];
+const images: string[] = [
+  "./assets/1.png",
+  "./assets/2.png",
+  "./assets/3.png",
+  "./assets/4.png",
+];
 
 interface Img {
-    src:string,
-    x:number,
-    y:number
-};
+  src: string;
+  x: number;
+  y: number;
+}
 
 let savedImg: Img[] = [];
 
-function initCanvas(){
-    if(main && canvas)
-    {
-        canvas.width = main.clientWidth;
-        canvas.height = main.clientHeight;
-    }
+function initCanvas() {
+  if (main && canvas) {
+    canvas.width = main.clientWidth;
+    canvas.height = main.clientHeight;
+  }
 }
 
-function drawImg(src:string, x:number, y:number){
-    const img = new Image();
-    img.src = images[Math.floor(Math.random()*images.length)]!;
-    img.onload = () => {
-         ctx?.drawImage(img, x, y, 100, 100);
-    }
+function drawImg(src: string, x: number, y: number) {
+  const img = new Image();
+  img.src = images[Math.floor(Math.random() * images.length)]!;
+  img.onload = () => {
+    ctx?.drawImage(img, x, y, 100, 100);
+  };
 }
-
 
 function addRandomImg() {
-    const random = images[Math.floor(Math.random()*images.length)]!;
+  const random = images[Math.floor(Math.random() * images.length)]!;
 
-    const x = Math.random() * (canvas.width - 100);
-    const y = Math.random() * (canvas.height - 100);
+  const x = Math.random() * (canvas.width - 100);
+  const y = Math.random() * (canvas.height - 100);
 
-    const newImg = {src: random, x:x, y:y};
-    savedImg.push(newImg);
+  const newImg = { src: random, x: x, y: y };
+  savedImg.push(newImg);
 
-    localStorage.setItem('savedImg', JSON.stringify(savedImg));
-    drawImg(newImg.src!, newImg.x, newImg.y);
-
+  localStorage.setItem("savedImg", JSON.stringify(savedImg));
+  drawImg(newImg.src!, newImg.x, newImg.y);
 }
 
-window.addEventListener('load', () => {
-    initCanvas();
-    const data = localStorage.getItem('savedImg');
-    if(data){
-        savedImg = JSON.parse(data);
-        savedImg.forEach(img => drawImg(img.src,img.x, img.y));
-    }
-})
+window.addEventListener("load", () => {
+  initCanvas();
+  const data = localStorage.getItem("savedImg");
+  if (data) {
+    savedImg = JSON.parse(data);
+    savedImg.forEach((img) => drawImg(img.src, img.x, img.y));
+  }
+});
 
 addImg.onclick = addRandomImg;
 
 function clearCanvas() {
-    ctx?.clearRect(0, 0, canvas.width, canvas.height);
-    savedImg =[];
-    localStorage.removeItem('savedImg');
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+  savedImg = [];
+  localStorage.removeItem("savedImg");
 }
 
 addImg.onclick = addRandomImg;
 clearImg.onclick = clearCanvas;
 
-//info window logic 
-const scrollEl = document.getElementById('scrollPos') as HTMLElement;
-const dimensionEl = document.getElementById('innerDim') as HTMLElement;
+//info window logic
+const scrollEl = document.getElementById("scrollPos") as HTMLElement;
+const dimensionEl = document.getElementById("innerDim") as HTMLElement;
 
-function updateState(){
-    const scrollX = Math.round(window.scrollX);
-    const scrollY = Math.round(window.scrollY);
+function updateState() {
+  const scrollX = Math.round(window.scrollX);
+  const scrollY = Math.round(window.scrollY);
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-    if(scrollEl){
-        scrollEl.innerText=`X: ${scrollX}, Y:${scrollY}`;
-    }
+  if (scrollEl) {
+    scrollEl.innerText = `X: ${scrollX}, Y:${scrollY}`;
+  }
 
-    if(dimensionEl){
-        dimensionEl.innerText=`${width}px x ${height}px`;
-    }
+  if (dimensionEl) {
+    dimensionEl.innerText = `${width}px x ${height}px`;
+  }
 }
 
-window.addEventListener('scroll', updateState);
-window.addEventListener('resize',updateState);
+window.addEventListener("scroll", updateState);
+window.addEventListener("resize", updateState);
 
 updateState();
